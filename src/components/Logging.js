209ddd67af5ba8fe3e089f8ae54273f39f9c69fc
ravid <ravid.mazon@checkmarx.com>
2022-07-28 17:11:17 +0300

    Removed flag from Logging.js

--- src/components/Logging.js
+++ src/components/Logging.js
@@ -11,10 +11,7 @@ function Logging() {
           height: '100vh',
         }}
       >
-        You have completed credential stuffing attack on the application! <br />
-        Due to the fact the app does not log and monitor anything, <br />
-        you are of the hook this time! <br />
-        Check out the logging endpoint in the backend (:
+        This app does not log and monitor anything. <br />
       </div>
     </>
   );
